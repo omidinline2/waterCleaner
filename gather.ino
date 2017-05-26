57e8631b3768e948c993543bbea749828f9067d4
omidinline2 <omidinline2@users.noreply.github.com>
2017-05-26 09:05:06 +0430

    Create gather.ino

---
+++ gather.ino
@@ -0,0 +1,108 @@
+//Sample using LiquidCrystal library
+#include <LiquidCrystal.h>
+ 
+// select the pins used on the LCD panel
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+ 
+// define some values used by the panel and buttons
+int lcd_key     = 0;
+int adc_key_in  = 0;
+#define btnRIGHT  0
+#define btnUP     1
+#define btnDOWN   2
+#define btnLEFT   3
+#define btnSELECT 4
+#define btnNONE   5
+ 
+// read the buttons
+int read_LCD_buttons()
+{
+ adc_key_in = analogRead(0);      // read the value from the sensor
+ // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
+ // we add approx 50 to those values and check to see if we are close
+ if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
+ if (adc_key_in < 50)   return btnRIGHT; 
+ if (adc_key_in < 195)  return btnUP;
+ if (adc_key_in < 380)  return btnDOWN;
+ if (adc_key_in < 555)  return btnLEFT;
+ if (adc_key_in < 790)  return btnSELECT;  
+ return btnNONE;  // when all others fail, return this...
+}
+
+
+ ///////////////////Flower 
+ volatile int NbTopsFan; //measuring the rising edges of the signal
+int Calc;                               
+int hallsensor =2;    //The pin location of the sensor
+
+void rpm ()     //This is the function that the interupt calls 
+{ 
+ NbTopsFan++;  //This function measures the rising and falling edge of the hall effect sensors signal
+} 
+/////////////////////////////
+void setup()
+{
+  //////////////// flower
+ pinMode(hallsensor, INPUT); //initializes digital pin 2 as an input
+ Serial.begin(9600); //This is the setup function where the serial port is initialised,
+ attachInterrupt(0, rpm, RISING); //and the interrupt is attached
+ ///////////////////////
+ lcd.begin(16, 2);              // start the library
+ lcd.setCursor(0,0);
+ lcd.print("Push the buttons"); // print a simple message
+}
+  
+void loop()
+{
+
+  //////////////flower
+ NbTopsFan = 0;      //Set NbTops to 0 ready for calculations
+ sei();            //Enables interrupts
+ delay (1000);      //Wait 1 second
+ cli();            //Disable interrupts
+ Calc = (NbTopsFan * 60 / 7.5); //(Pulse frequency x 60) / 7.5Q, = flow rate in L/hour 
+  lcd.setCursor(6,1);            // move cursor to second line "1" and 9 spaces over
+ lcd.print (Calc, DEC); //Prints the number calculated above
+ lcd.print (" L/H\r\n"); //Prints "L/hour" and returns a  new line
+
+  //////////////////////
+  
+ 
+ lcd.setCursor(0,1);            // move to the begining of the second line
+ lcd_key = read_LCD_buttons();  // read the buttons
+ 
+ switch (lcd_key)               // depending on which button was pushed, we perform an action
+ {
+   case btnRIGHT:
+     {
+     lcd.print("RIGHT ");
+     break;
+     }
+   case btnLEFT:
+     {
+     lcd.print("LEFT   ");
+     break;
+     }
+   case btnUP:
+     {
+     lcd.print("UP    ");
+     break;
+     }
+   case btnDOWN:
+     {
+     lcd.print("DOWN  ");
+     break;
+     }
+   case btnSELECT:
+     {
+     lcd.print("SELECT");
+     break;
+     }
+     case btnNONE:
+     {
+     lcd.print("NONE  ");
+     break;
+     }
+ }
+ 
+}
